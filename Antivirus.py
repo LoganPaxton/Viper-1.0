a803f100ac4ee28928bf2debb8a6120226f73305
LoganPaxton <111173907+LoganPaxton@users.noreply.github.com>
2023-03-05 15:34:26 -0500

    V1.0

---
+++ Antivirus.py
@@ -0,0 +1,24 @@
+import time
+print("Viper")
+print("Made by LoganPaxton")
+print("Loading Resources")
+time.sleep(5)
+print("Resources loaded sucsessfully!")
+print("WARNING! FEATURES MAY NOT WORK AS INTENDED!")
+time.sleep(1)
+
+detection = False
+
+malicious = ("rd c:\system32", "del c:\", "rd c:\")
+
+filename = input("Batch File >> ")
+fs = open(filename, 'r')
+batch = fs.read()
+fs.close()
+codes = batch.split("\n")
+for line in codes:
+   for maliciouscode in malicious:
+       if maliciouscode.lower() == line.lower():
+           detection = True
+if detection == True:
+    print("Virus detected!")
